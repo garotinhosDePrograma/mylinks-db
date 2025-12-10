47a7656c36218fe0557ddab1e95f422007d57f6b
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-12-09 22:37:51 -0300

    Update user passwords and insert links

--- MyLinks.sql
+++ MyLinks.sql
@@ -19,9 +19,9 @@ CREATE TABLE IF NOT EXISTS links (
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
-INSERT INTO usuarios (username, email, senha, foto_perfil) VALUES ('Harry Potter', 'harry@hogwarts.com', 'harry123', '');
-INSERT INTO usuarios (username, email, senha, foto_perfil) VALUES ('Peter Parker', 'miranha@gmail.com', 'peter123', '');
-INSERT INTO usuarios (username, email, senha, foto_perfil) VALUES ('Tony Stark', 'ironman@hotmail.com', 'tony123', '');
+INSERT INTO usuarios (username, email, senha, foto_perfil) VALUES ('Harry Potter', 'harry@hogwarts.com', '$2b$12$abZUAfItYPmCe8.2donVJeWv4BzUcyR51jWLz03wLet6u8hh4ReMW', '');
+INSERT INTO usuarios (username, email, senha, foto_perfil) VALUES ('Peter Parker', 'miranha@gmail.com', '$2b$12$tcroM4CX995wiZH/JtJgUeGSCm3LgRDNJGsX/u4wVAU7bmkxF8poy', '');
+INSERT INTO usuarios (username, email, senha, foto_perfil) VALUES ('Tony Stark', 'ironman@hotmail.com', '$2b$12$9YnZzvk2pEngqRPAwu43Qu7WwMDD9AsyaRntlMbtfxCw/6sW.43Ay', '');
 
 INSERT INTO links (usuario_id, titulo, url, ordem) VALUES (1, 'Harry Potter', 'https://m.youtube.com/watch?v=jAxvLkfeCpI&pp=0gcJCR4Bo7VqN5tD', 1);
 INSERT INTO links (usuario_id, titulo, url, ordem) VALUES (2, 'Miranha', 'https://www.reddit.com/r/Spiderman/comments/t0emam/which_episode_of_spiderman_1960_is_this_meme_from/?tl=pt-br', 1);
